975cc3b572d26c147294d731b8c3baefd5613c9c
UGEL4 <1365972041@qq.com>
2023-06-28 00:19:09 +0800

    update

--- SimpleGPU/Source/include/render_graph/include/Graph_Boost.hpp
+++ SimpleGPU/Source/include/render_graph/include/Graph_Boost.hpp
@@ -4,10 +4,22 @@
 
 namespace BoostGraph
 {
-    template < class VertexProperty = boost::no_property,
-        class EdgeProperty = boost::no_property, class GraphProperty = boost::no_property,
-        class EdgeListS = boost::listS >
+    template <
+        class VertexProperty = boost::no_property,
+        class EdgeProperty   = boost::no_property,
+        class GraphProperty  = boost::no_property,
+        class EdgeListS      = boost::listS>
     using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, VertexProperty, EdgeProperty, GraphProperty, EdgeListS>;
 
-    //boost::graph_traits<Graph<
+    template <
+        class VertexProperty = boost::no_property,
+        class EdgeProperty   = boost::no_property,
+        class GraphProperty  = boost::no_property>
+    using GraphVertex = typename boost::graph_traits<Graph<VertexProperty, EdgeProperty, GraphProperty>>::vertex_descriptor;
+
+    template <
+        class VertexProperty = boost::no_property,
+        class EdgeProperty   = boost::no_property,
+        class GraphProperty  = boost::no_property>
+    using GraphEdge = typename boost::graph_traits<Graph<VertexProperty, EdgeProperty, GraphProperty>>::edge_descriptor;
 }
